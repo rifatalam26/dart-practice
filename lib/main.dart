270b084ffa5e2bb6327c3f3c94f0c4308345e136
rifat <mdrftlm@gmail.com>
2024-11-21 22:48:22 +0600

    dart practice

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+
+/* void main() {
+    int a=10;
+    int b=30;
+    int c=20;
+    if(a>b && a>c){
+        print('a is Big');
+     }else if(b>a && b>c){
+            print('b is Big');
+        }else{
+            print('c is Big');
+        }
+    
+} */
+// void main(){
+//     for(int i=1; i<10; i++);
+//     print('The num')
+// }
+//
+
+/*void main() {
+ List data=['rifat','alam','najmin',27,true];
+  
+  var search='alam';
+  
+  for(int i=0; i<data.length; i++){
+    
+    if(data[i]==search){
+    
+    print('$search found in index: $i');
+  }
+}
+}  */
+
+/* void main(){
+List data=['alam','rifat','sumi',49,true];
+
+var search=true;
+
+for(var i in data){
+
+  if(i==search){
+
+    print('$search is found in index : $i');
+  }
+}
+
+} */
+void main() {
+  int search=9;
+ 
+  for(int i=1; i<=10; i++){
+    
+    print('${i} X $search=${(i)*search}');
+  }
+}
+
