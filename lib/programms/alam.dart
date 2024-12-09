6c8555aff4082086111055bc9718321364f9d107
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-09 23:48:51 +0600

    find largest number

--- lib/programms/alam.dart
+++ lib/programms/alam.dart
@@ -1,16 +1,21 @@
 // Find the Sum of Elements in a List
+// void main(){
+//   List num=[1,2,3,4,5];
+//   var sum=0;
+
+
+//   for(int i=num.first; i<=num.last; i++){
+//     sum+=i;
+//   }
+//   print("$sum");
+// }
+
+ //Find the Largest of Two Numbers
 void main(){
-  List num=[1,2,3,4,5];
-  var sum=0;
-
-
-  for(int i=num.first; i<=num.last; i++){
-    sum+=i;
-  }
-  print("$sum");
+  int a=151;
+  int b=154;
+print("the largest number is ${a>b ? a : b}");
 }
-
-
 
 
 
