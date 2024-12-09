e8afb3fd6ce5b9a3b8d7b2a3b9b5b223882747c7
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-09 22:32:22 +0600

    List sum using for in loop

---
+++ lib/programms/alam.dart
@@ -0,0 +1,24 @@
+// Find the Sum of Elements in a List
+void main(){
+  List num=[1,2,3,4,5];
+  var sum=0;
+  for(int number in num){
+    sum+=number;
+  }
+  print(sum);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
