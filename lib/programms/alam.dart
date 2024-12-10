fe5a59fda48259cacd046b6be1a2dd5950206168
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-11 00:24:00 +0600

    List add

--- lib/programms/alam.dart
+++ lib/programms/alam.dart
@@ -106,18 +106,25 @@
 //Write a Dart code that takes this list and makes a new list
 // that has only the even elements of this list in it.
 
+// void main(){
+//   List a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+//   List b=[];
+//   for(int i in a){
+//     if(i % 2==0){
+//       b.add(i);
+//     }
+//   }
+// print(b);
+// }
+
 void main(){
-  List a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
-  List b=[];
-  for(int i in a){
-    if(i % 2==0){
-      b.add(i);
-    }
-  }
-print(b);
+ List a = [5, 10, 15, 20, 25];
+ List b=[];
+ b.add(a.first);
+   b.add(a.last);
+ print(b);
+
 }
-
-
 
 
 
